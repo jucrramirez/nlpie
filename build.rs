3c375538d6534ab608e10198f18b245ef4426557
Manuel R.C. <manuel.ramirez94@pm.me>
2026-07-16 22:54:20 +0200

    chore: BIAS dep in the actions

--- build.rs
+++ build.rs
@@ -3,4 +3,10 @@ fn main() {
     {
         println!("cargo:rustc-link-lib=framework=Accelerate");
     }
+    #[cfg(all(target_os = "linux", target_arch = "x86_64"))]
+    {
+        println!("cargo:rustc-link-search=/usr/lib/x86_64-linux-gnu");
+        println!("cargo:rustc-link-lib=dylib=openblas");
+        println!("cargo:rustc-link-lib=dylib=gfortran");
+    }
 }
